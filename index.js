25c0c1a93d9cda141883444af6404aed0e903a94
sanaatan7 <karmakarsanatan0791@gmail.com>
2026-04-17 20:06:28 +0530

    init express server

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("I am Sanatan");
+import dotenv from "dotenv";
+import app from "./src/app.js";
+dotenv.config({
+  path: "./.env",
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`app listing on port ${port}`);
+});
