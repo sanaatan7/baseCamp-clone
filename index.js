69165e671fee600f3e348f6cc2b9565693ec9d9b
sanaatan7 <karmakarsanatan0791@gmail.com>
2026-04-18 15:52:10 +0530

    add database connection

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 import dotenv from "dotenv";
 import app from "./src/app.js";
+import connectDB from "./src/db/index.js";
 dotenv.config({
   path: "./.env",
 });
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
-  console.log(`app listing on port ${port}`);
-});
+connectDB()
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`app listing on port ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.error("MonGo Db connection error", err);
+  });
