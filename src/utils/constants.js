e93f3103c2042a4601e1bb1d836c7e41fdf73e6a
sanaatan7 <karmakarsanatan0791@gmail.com>
2026-04-18 15:50:44 +0530

    .

---
+++ src/utils/constants.js
@@ -0,0 +1,15 @@
+export const UserRolesEnum = {
+  ADMIN: "admin",
+  PROJECT_ADMIN: "project_admin",
+  MEMBER: "member",
+};
+
+export const AvaiableUserRoles = Object.values(UserRolesEnum);
+
+export const taskStatusEnum = {
+  TODO: "todo",
+  IN_PROGRESS: "in_progress",
+  DONE: "done",
+};
+
+export const AvailableTaskStatus = Object.values(taskStatusEnum);
